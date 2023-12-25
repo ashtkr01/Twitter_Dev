af8a1b0951526cf223aafe68f7468c51590f5b0e
ashtkr01 <ashishkumarkatheriyaa@gmail.com>
2023-12-26 00:07:24 +0530

    Added basic crud repo and extended in like repo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,6 +1,11 @@
 import Tweet from '../models/tweet.js';
+import CrudRepository from './crud-repository.js';
 
-class TweetRepository{
+class TweetRepository extends CrudRepository{
+
+    constructor(){
+        super(Tweet);
+    }
 
     async create(data) {
         try {
@@ -11,28 +16,10 @@ class TweetRepository{
         }
     }
 
-    async get(id){
-        try {
-            const tweet = await Tweet.findById(id);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     async getWithComments(id){
         try {
             /*lean() is a method that you can chain to a Mongoose query to improve query performance by returning plain JavaScript objects instead of Mongoose documents.*/
             const tweet = await Tweet.findById(id).populate({path : 'comments'}).lean();
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    
-    async destroy(tweetId){
-        try {
-            const tweet = await Tweet.findByIdAndDelete(tweetId);
             return tweet;
         } catch (error) {
             console.log(error);
