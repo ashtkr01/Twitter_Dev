bed975f514ca75e18bb3ff3221ca1cea1ea7ab69
ashtkr01 <ashishkumarkatheriyaa@gmail.com>
2023-12-25 13:51:02 +0530

    Added the functionality of conversion of hashtag to small and then do operation and also remove the hashtag array from Tweet schema

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { connect } from './config/database.js';
 
-import TweetService from './services/tweet-service.js';
+// import TweetService from './services/tweet-service.js';
 
 const app = express();
 
@@ -12,11 +12,11 @@ app.listen( async () =>{
 
     await connect();
 
-    let tweetService = new TweetService();
-    let tweet = await tweetService.create({
-        content: 'I am a bhakt of #rana , #sangha , #amar '
-    });
-    console.log(tweet);
+    // let tweetService = new TweetService();
+    // let tweet = await tweetService.create({
+    //     content: 'I am a bhakt of #rana , #sangha , #amar  '
+    // });
+    // console.log(tweet);
 
     console.log('Database got Connected');
 });
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -8,7 +8,9 @@ class TweetService{
     //Create:
     async create(data){
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1));
+        const tags = content.match(/#[a-zA-Z0-9_]+/g)
+                                                    .map((tag) => tag.substring(1))
+                                                    .map((tag) => tag.toLowerCase());
         const tweet = await this.tweetRepository.create(data);
         let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
         let titleOfPresenttags = alreadyPresentTags.map(tags => tags.title);
@@ -17,20 +19,11 @@ class TweetService{
             return {title: tag, tweets: [tweet.id]};
         });
         const response = await this.hashtagRepository.bulkCreate(newTags);
-        //Add the hashtagId in Tweet:
-        for(const tag of response){
-            tweet.hashtags.push(tag.id);
-            await tweet.save();
-        }
         //Why we don't use await before save : Beacause this thing can go parallely and we don't need to wait for it:
         alreadyPresentTags.forEach((tag) =>{
             tag.tweets.push(tweet.id);
             tag.save();
         });
-        for(const tag of alreadyPresentTags){
-            tweet.hashtags.push(tag.id);
-            await tweet.save();
-        }
         return tweet;
     }
 }
