860ab1fad91cda85569e405946528f567b3409c0
ashtkr01 <ashishkumarkatheriyaa@gmail.com>
2023-12-25 14:44:53 +0530

    Added controllers and routes to create tweet api

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import { connect } from './config/database.js';
-
-// import TweetService from './services/tweet-service.js';
-
-const app = express();
 
 const PORT = 3000;
 
-app.listen( async () =>{
+import apiRoutes from './routes/index.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use('/api', apiRoutes);
+
+
+
+app.listen(PORT, async () =>{
     console.log(`Started listen at ${PORT}`);
 
     await connect();
-
-    // let tweetService = new TweetService();
-    // let tweet = await tweetService.create({
-    //     content: 'I am a bhakt of #rana , #sangha , #amar  '
-    // });
-    // console.log(tweet);
 
     console.log('Database got Connected');
 });
